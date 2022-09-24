c8491612caf9b72748f129e4d94d8c5c314d98b5
Chintan Patel <patelchintan843@gmail.com>
2022-09-24 13:21:51 +0530

    upd insertionsort

--- Sorting_Alogorithm/InsertionSort.cpp
+++ Sorting_Alogorithm/InsertionSort.cpp
@@ -10,22 +10,22 @@ void displayArray(int *arr, int n)
     cout << endl;
 }
 
-void insertionSort(int *arr, int n)
+void insertionSort(int *a, int n)
 {
     int key;
     int j;
     // It starts from 1 because one variable is already sorted
     for (int i = 1; i < n; i++)
     {
-
-        key = arr[i];
-        j = i - 1;
-        while (arr[j] > key && j >= 0)
+        // 7 8 6 5 7
+        key = a[i]; // 8 6 5 7
+        j = i - 1;  // 0 1 2 3
+        while (a[j] > key && j >= 0)
         {
-            arr[j + 1] = arr[j];
-            j--;
+            a[j + 1] = a[j]; // 7 7 8 6
+            j--;             // 1 0 -1
         }
-        arr[j + 1] = key;
+        a[j + 1] = key; // 5 6 7 8 7
     }
 }
 

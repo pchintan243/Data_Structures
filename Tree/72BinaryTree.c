084a95b0153e28876ccf9b23925e624261a4a959
Chintan Patel <patelchintan843@gmail.com>
2022-09-25 20:06:17 +0530

    add:searchbinarytree file

---
+++ Tree/72BinaryTree.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    p->data = data;
+    p->left = NULL;
+    p->right = NULL;
+    return p;
+};
+
+void inOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+int isBST(struct Node *root)
+{
+    /*
+             4
+           /   \
+          2      6
+        /  \    /  \
+       1   3   5    7
+
+    */
+    static struct Node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    struct Node *p = createNode(5);
+    struct Node *p1 = createNode(3);
+    struct Node *p2 = createNode(6);
+    struct Node *p3 = createNode(1);
+    struct Node *p4 = createNode(4);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    // inOrder(p);
+    // isBST(p);
+    printf("\n%d\n", isBST(p));
+    return 0;
+}

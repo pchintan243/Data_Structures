a78b61a3a4f223e04f4b9a90d91addd150787b3e
Chintan Patel <patelchintan843@gmail.com>
2022-10-05 15:42:30 +0530

    add:deletioninBST

---
+++ Tree/78BSTDeletion.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    p->data = data;
+    p->left = NULL;
+    p->right = NULL;
+    return p;
+};
+
+void inOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+struct Node *inOrderPredecessor(struct Node *root)
+{
+    root = root->left;
+    while (root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+struct Node *deleteNode(struct Node *root, int key)
+{
+    struct Node *iPre;
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        free(root);
+        return NULL;
+    }
+
+    // Search for the node to be deleted
+    if (key < root->data)
+    {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->data)
+    {
+        root->right = deleteNode(root->right, key);
+    }
+
+    // Deletion strategy when the node is found
+    else
+    {
+        iPre = inOrderPredecessor(root);
+        root->data = iPre->data;
+        root->left = deleteNode(root->left, iPre->data);
+    }
+    return root;
+}
+
+int main()
+{
+    struct Node *p = createNode(5);
+    struct Node *p1 = createNode(3);
+    struct Node *p2 = createNode(6);
+    struct Node *p3 = createNode(1);
+    struct Node *p4 = createNode(4);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    inOrder(p);
+    cout << endl;
+    deleteNode(p, 6);
+    inOrder(p);
+    return 0;
+}

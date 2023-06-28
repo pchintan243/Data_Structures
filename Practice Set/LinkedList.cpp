92747abe558eb3039244740b9dde4a1cb0c1b0ca
chintan <patelchintan843@gmail.com>
2023-06-28 21:02:06 +0530

    deleted in linkedlist

--- Practice Set/LinkedList.cpp
+++ Practice Set/LinkedList.cpp
@@ -71,6 +71,70 @@ struct Node *insertAfterNode(struct Node *ptr, struct Node *prev, int data)
 
     return ptr;
 }
+
+struct Node *deleteBegin(struct Node *ptr)
+{
+    struct Node *q = ptr;
+    ptr = ptr->next;
+    free(q);
+
+    return ptr;
+}
+
+struct Node *deleteEnd(struct Node *ptr)
+{
+    struct Node *q = ptr->next;
+    struct Node *p = ptr;
+
+    while (q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    p->next = q->next;
+    free(q);
+
+    return ptr;
+}
+
+struct Node *deleteIndex(struct Node *ptr, int index)
+{
+    struct Node *q = ptr->next;
+    struct Node *p = ptr;
+
+    int i = 1;
+
+    while (index != i)
+    {
+        p = p->next;
+        q = q->next;
+        i++;
+    }
+    p->next = q->next;
+    free(q);
+
+    return ptr;
+}
+
+struct Node *deleteByValue(struct Node *ptr, int value)
+{
+    struct Node *q = ptr->next;
+    struct Node *p = ptr;
+
+    while (q->next != NULL && q->data != value)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    if (q->data == value)
+    {
+        p->next = q->next;
+        free(q);
+    }
+
+    return ptr;
+}
+
 int main()
 {
     struct Node *head = (struct Node *)malloc(sizeof(struct Node));
@@ -86,20 +150,36 @@ int main()
 
     display(head);
 
-    cout << "---" << endl;
-    insertAfterNode(head, third, 200);
-    display(head);
+    // cout << "---" << endl;
+    // insertAfterNode(head, third, 200);
+    // display(head);
+
+    // cout << "---" << endl;
+    // head = insertBegin(head, 34);
+    // display(head);
+
+    // cout << "---" << endl;
+    // insertEnd(head, 89);
+    // display(head);
+
+    // cout << "---" << endl;
+    // insertBetween(head, 100, 2);
+    // display(head);
+
+    // cout << "---" << endl;
+    // head = deleteBegin(head);
+    // display(head);
+
+    // cout << "---" << endl;
+    // deleteEnd(head);
+    // display(head);
+
+    // cout << "---" << endl;
+    // deleteIndex(head, 1);
+    // display(head);
 
     cout << "---" << endl;
-    head = insertBegin(head, 34);
-    display(head);
-
-    cout << "---" << endl;
-    insertEnd(head, 89);
-    display(head);
-
-    cout << "---" << endl;
-    insertBetween(head, 100, 2);
+    deleteByValue(head, 55);
     display(head);
 
     return 0;

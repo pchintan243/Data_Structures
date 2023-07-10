0caedfa52d6a13831f3ea78c1b1837053676f91d
chintan <patelchintan843@gmail.com>
2023-07-10 17:31:36 +0530

    Bubble sort practice

---
+++ Practice Set/Bubble.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+void display(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+void bubbleSort(int *arr, int n)
+{
+    bool flag = true;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i; j < n - i - 1; j++)
+        {
+            if (arr[j + 1] < arr[j])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                flag = false;
+            }
+        }
+        if (flag)
+        {
+            break;
+        }
+    }
+    display(arr, n);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of an array: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the " << i + 1 << "the element: ";
+        cin >> arr[i];
+    }
+    bubbleSort(arr, n);
+
+    return 0;
+}

532212d4f47019466197c2b8f168150886ce2290
chintan <patelchintan843@gmail.com>
2023-08-09 11:24:32 +0530

    find all frequency

---
+++ Practice Set/AllFreq.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s = "7uuyf  uy";
+    int a[256] = {0};
+    for (int i = 0; i < s.length(); i++)
+    {
+        a[s[i]]++;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if (a[i] != 0)
+        {
+            cout << char(i) << ":" << a[i] << " ";
+        }
+    }
+    return 0;
+}

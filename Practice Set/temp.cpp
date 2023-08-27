81f068f0cc34c8af275a9f7f4981d16f67b0537e
chintan <patelchintan843@gmail.com>
2023-08-27 15:11:27 +0530

    check i/p o/p logic

---
+++ Practice Set/temp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+typedef unsigned long long int ll;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    string t;
+    cin >> t;
+    cout << t;
+    return 0;
+}
